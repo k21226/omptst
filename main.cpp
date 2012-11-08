dc27a1f866cb6459be2d1d794e0c658145d208fd
Osipov Alexey <spinor@ct-qmc.org>
2012-11-08 16:32:51 +0400

    Calculating of average over a matrix in parrallel way

--- main.cpp
+++ main.cpp
@@ -7,36 +7,32 @@
 #include <math.h>
 
 double stoptime(); 
-void disp();
+double AvgM();
 double avg, avg2;
 int N, NN;
 double** M;
 
 int main(){
 	int itt = 100;
-	double t = stoptime(); 
+	double Avg = .0;
 	N = pow(2, 10); NN = N*N;
 	srand(time(NULL));
 	M = new double* [N];
 	for(int i = 0; i < N; i++){M[i] = new double[N];}
-	for(int p = 0; p < itt; p++) disp();
+	for(int ii = 0; ii < NN; ii++){int i = ii/N; int j = ii%N; M[i][j] = 1.*rand()/RAND_MAX - .5;}
+	double t = stoptime();
+	#pragma omp parallel for reduction(+:Avg)
+	for(int p = 0; p < itt; p++) Avg += AvgM();
 	t = stoptime() - t;
-	std::cout << t << "\t" << avg << "\t" << avg2 <<std::endl; 
+	std::cout << t << "\t" << Avg/itt <<std::endl; 
 	delete(M);
 	return 0;
 }
-void disp(){
-	#pragma omp parallel for
-	for(int ii = 0; ii < NN; ii++){int i = ii/N; int j = ii%N; M[i][j] = 1.*rand()/RAND_MAX - .5;}
-	avg = .0; avg2 = .0;	
-	#pragma omp parallel for reduction(+:avg)
+double AvgM(){
+	avg = .0; 
 	for(int ii = 0; ii < NN; ii++){int i = ii/N; int j = ii%N; avg += M[i][j];}
 	avg /= NN;
-	#pragma omp parallel for
-	for(int ii = 0; ii < NN; ii++){int i = ii/N; int j = ii%N; M[i][j] -= avg;}
-	#pragma omp parallel for reduction(-:avg2)
-	for(int ii = 0; ii < NN; ii++){int i = ii/N; int j = ii%N; avg2 += M[i][j]*M[i][j];}
-	avg2 /= NN;
+	return avg;
 }
 double stoptime(){
    struct timeval t;
